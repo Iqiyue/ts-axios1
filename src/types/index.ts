ebc5ba6e71a6b01405c446f9e741e5dc5414c40e
时利卫 <1062204452@qq.com>
2021-06-03 07:50:09 +0800

    feat:泛型设置默认值

--- src/types/index.ts
+++ src/types/index.ts
@@ -22,7 +22,7 @@ export interface AxiosRequestConfig {
   responseType?: XMLHttpRequestResponseType
   timeout?: number
 }
-export interface AxiosResponse<T> {
+export interface AxiosResponse<T = any> {
   data: T
   status: number
   statusText: string
@@ -30,30 +30,30 @@ export interface AxiosResponse<T> {
   config: AxiosRequestConfig
   request: any
 }
-export interface AxiosPromise<T> extends Promise<AxiosResponse<T>> {}
+export interface AxiosPromise<T = any> extends Promise<AxiosResponse<T>> {}
 
 // todo 创建AxiosError类型接口
 
-export interface AxiosError<T> extends Error {
+export interface AxiosError extends Error {
   config: AxiosRequestConfig
   code?: string
   request?: any
-  response?: AxiosResponse<T>
+  response?: AxiosResponse
   isAxiosError: boolean
 }
 
 // todo  首先定义一个Axios 类型接口,他描述了Axios类中的公共方法,接着定义了AxiosInstance接口,继承了Axios,他就是一个混合类型的接口
 export interface Axios {
-  request<T>(config: AxiosRequestConfig): AxiosPromise<T>
-  get<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
-  delete<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
-  head<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
-  options<T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
-  post<T>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
-  put<T>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
-  patch<T>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  request<T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+  get<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  delete<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  head<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  options<T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  post<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  put<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
+  patch<T = any>(url: string, data?: any, config?: AxiosRequestConfig): AxiosPromise<T>
 }
 export interface AxiosInstance extends Axios {
-  <T>(config: AxiosRequestConfig): AxiosPromise<T>
-  <T>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
+  <T = any>(config: AxiosRequestConfig): AxiosPromise<T>
+  <T = any>(url: string, config?: AxiosRequestConfig): AxiosPromise<T>
 }
